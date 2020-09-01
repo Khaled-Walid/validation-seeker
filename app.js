7b7014848f95ac6744b801707c41d762b6c585b8
Khaled Walid <justkhwf@outlook.com>
2020-09-01 21:13:26 +0200

    beta

---
+++ app.js
@@ -0,0 +1,82 @@
+const startingButton = document.getElementById("starting-button");
+const startingScreen = document.getElementById("starting-screen");
+const chooseScreen = document.getElementById("choose-screen");
+const gameScreen = document.getElementById("game-screen");
+
+let isHeads = true;
+let coin;
+let counterHeads = 0;
+let counterTails = 0;
+let bestCounter = 1;
+
+const headsButton = document.getElementById("heads");
+const tailsButton = document.getElementById("tails");
+const flipButton = document.getElementById("flip");
+
+const headsCounter = document.getElementById("heads-counter");
+const tailsCounter = document.getElementById("tails-counter");
+
+const finishingScreen = document.getElementById("finishing-screen");
+const againButton = document.getElementById("again-button");
+const bestOf = document.getElementById("best-of")
+
+function hide(section1, section2) {
+  section1.classList.toggle("hide");
+  section2.classList.toggle("hide");
+}
+startingButton.addEventListener(
+  "click",
+  hide.bind(null, startingScreen, chooseScreen)
+);
+
+function hide2(section1, section2, choice) {
+  hide(section1, section2);
+  isHeads = choice;
+  counterHeads=0
+  counterTails=0
+  bestCounter=1
+  headsCounter.innerHTML = `${counterHeads}`
+  tailsCounter.innerHTML = `${counterTails}`
+  bestOf.innerHTML=`${bestCounter}`
+}
+
+headsButton.addEventListener(
+  "click",
+  hide2.bind(null, chooseScreen, gameScreen, true)
+);
+tailsButton.addEventListener(
+  "click",
+  hide2.bind(null, chooseScreen, gameScreen, false)
+);
+againButton.addEventListener(
+  "click",
+  hide.bind(null, chooseScreen, finishingScreen)
+);
+
+function flip() {
+  let userCounter;
+  let pcCounter;
+  const random = Math.random();
+  if (random >= 0.5) {
+    coin = true;
+    counterHeads++;
+    headsCounter.innerHTML = `${counterHeads}`;
+  } else {
+    coin = false;
+    counterTails++;
+    tailsCounter.innerHTML = `${counterTails}`;
+  }
+  userCounter = isHeads ? counterHeads : counterTails;
+  pcCounter = !isHeads ? counterHeads : counterTails;
+  ++bestCounter
+  if (bestCounter%2!==0){
+    bestOf.innerHTML=`${bestCounter}`
+  }
+
+  
+  if (userCounter > pcCounter) {
+    hide(gameScreen, finishingScreen);
+  }
+}
+
+flipButton.addEventListener("click", flip);
